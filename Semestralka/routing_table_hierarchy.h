b00a91f97cd30d71cf2b33fa2bc00df6093efcaa
MarosVorcak <vorcak10@stud.uniza.sk>
2025-04-14 17:50:49 +0200

    hierarchy init works. 1/3 parts done nehehehe

--- Semestralka/routing_table_hierarchy.h
+++ Semestralka/routing_table_hierarchy.h
@@ -5,97 +5,108 @@
 #include <string>
 
 class RTNode {
-	private:
-		int octetValue;
-	public:
-		RTNode();
-		RTNode(int octetValue);
-		void setOctetValue(int octetValue);
-		int getOctetValue();
-};
-
-class FinalRTNode : public RTNode {
-	private:
-		std::vector<RoutingRecord*> recordPointers;
-	public:
-		void addPointer(RoutingRecord* pointer);
+private:
+    int octetValue;
+    std::vector<RoutingRecord*> pointerToRecords;
+public:
+    RTNode();
+    RTNode(int octetValue);
+    void setOctetValue(int octetValue);
+    int getOctetValue();
+    void addPointer(RoutingRecord* pointer);
+    bool operator==(const RTNode& other) const;
+    RTNode& operator=(const RTNode& other);
 };
 
 class RoutingTable {
-	private:
-		ds::amt::MultiWayExplicitHierarchy<RTNode> hierarchy;
-		std::vector<RoutingRecord> loadedRecords;
-	public:
-		RoutingTable(const std::string& fileName);
-		void initHierarchy(const std::vector<RoutingRecord>& loadedRecords);
-		void addIP(const std::string& ip, RoutingRecord* pointerToRecord);
+private:
+    ds::amt::MultiWayExplicitHierarchy<RTNode> hierarchy;
+    std::vector<RoutingRecord> loadedRecords;
+public:
+    RoutingTable(const std::string& fileName);
+    void initHierarchy(const std::vector<RoutingRecord>& loadedRecords);
+    void addIP(const std::string& ip, RoutingRecord* pointerToRecord);
+    void printSize();
 };
 
 RTNode::RTNode() {
-	this->octetValue = 0;
+    this->octetValue = 0;
 }
 
 RTNode::RTNode(int octetValue) {
-	this->octetValue = octetValue;
+    this->octetValue = octetValue;
 }
+
 void RTNode::setOctetValue(int octetValue) {
-	this->octetValue = octetValue;
+    this->octetValue = octetValue;
 }
 
 int RTNode::getOctetValue() {
-	return this->octetValue;
+    return this->octetValue;
 }
 
-void FinalRTNode::addPointer(RoutingRecord* pointer) {
-	this->recordPointers.push_back(pointer);
-};
+void RTNode::addPointer(RoutingRecord* pointer) {
+    this->pointerToRecords.push_back(pointer);
+}
+
+bool RTNode::operator==(const RTNode& other) const {
+    return this->octetValue == other.octetValue;
+}
+
+RTNode& RTNode::operator=(const RTNode& other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->octetValue = 0;
+    this->pointerToRecords.clear();
+    this->octetValue = other.octetValue;
+    this->pointerToRecords = other.pointerToRecords;
+    return *this;
+}
 
 RoutingTable::RoutingTable(const std::string& fileName) {
-	this->loadedRecords = parseCSV(fileName);
-	this->initHierarchy(loadedRecords);
+    this->loadedRecords = parseCSV(fileName);
+    this->initHierarchy(loadedRecords);
 }
 
 void RoutingTable::initHierarchy(const std::vector<RoutingRecord>& loadedRecords) {
-	this->hierarchy.emplaceRoot();
-	for (RoutingRecord record : loadedRecords) {
-		this->addIP(record.getPrefixAdd(), &record);
-	}
+    this->hierarchy.emplaceRoot().data_ = RTNode();
+    for (RoutingRecord record : loadedRecords) {
+        this->addIP(record.getPrefixAdd(), &record);
+    }
 }
 
 void RoutingTable::addIP(const std::string& ip, RoutingRecord* pointerToRecord) {
-	std::stringstream ss(ip);
-	std::string octet;
-	std::vector<int> octets;
+    std::stringstream ss(ip);
+    std::string octet;
+    std::vector<int> octets;
 
-	while (std::getline(ss, octet, '.')) {
-		octets.push_back(std::stoi(octet));
-	}
+    while (std::getline(ss, octet, '.')) {
+        octets.push_back(std::stoi(octet));
+    }
 
-	if (octets.size() != 4) {
-		throw std::invalid_argument("Invalid IP format");
-	}
-	auto currentNode = this->hierarchy.accessRoot();
-	for (int i = 0; i < 4; ++i) {
-		if (currentNode->sons_->size() == 0) {
-			for (int j = 0; j < 4; ++j) {
-				int octetValue = octets[i];
+    decltype(auto) currentNode = this->hierarchy.accessRoot();
+    for (int currentOctet : octets) {
+        bool found = false;
 
-				if (j == 3) {
-					auto newNode = this->hierarchy.emplaceSon(*currentNode,currentNode->sons_->size()+1);
-					currentNode = &newNode;
-					FinalRTNode* finalNode = dynamic_cast<FinalRTNode*>(*currentNode);
-					if (finalNode) {
-						finalNode->addPointer(pointerToRecord);
-					}
+        for (decltype(auto) son : *currentNode->sons_) {
+            if (son->data_.getOctetValue() == currentOctet) {
+                currentNode = son;
+                found = true;
+                break;
+            }
+        }
 
-				}
-
-			}
-
-			return;
-		}
-		int currentOctet = octets[i];
-		bool found;
-	}
+        if (!found) {
+            size_t index = currentNode->sons_->size();
+            decltype(auto) newNode = this->hierarchy.emplaceSon(*currentNode, index);
+            newNode.data_ = RTNode(currentOctet);
+            currentNode = &newNode;
+        }
+    }
+    currentNode->data_.addPointer(pointerToRecord);
 }
 
+void RoutingTable::printSize() {
+    std::cout << "Current size is: " << this->hierarchy.size();
+}
--- Semestralka/semestralka_main.cpp
+++ Semestralka/semestralka_main.cpp
@@ -1,112 +1,119 @@
 #include <iostream>
-#include "csv_parser.h"
-#include "filter_tools.h"
-#include "time_tools.h"
-
-void showMenu() {
-    std::cout << "======Menu======" << '\n';
-    std::cout << "1. Filter by lifetime" << '\n';
-    std::cout << "2. Filter by nexthop address" << '\n';
-    std::cout << "3. Filter by matching with prefix address" << '\n';
-    std::cout << "4. Print filtered data structure" << '\n';
-    std::cout << "5. Reset all filters" << '\n';
-    std::cout << "6. Exit the program" << '\n';
-    std::cout << "Enter your choice: ";
-}
-
-void printFiltered(std::vector<RoutingRecord>& filteredVector) {
-    for (RoutingRecord& record : filteredVector) {
-        record.print();
-    }
-}
+//#include "csv_parser.h"
+//#include "filter_tools.h"
+//#include "time_tools.h"
+#include "routing_table_hierarchy.h"
+//void showMenu() {
+//    std::cout << "======Menu======" << '\n';
+//    std::cout << "1. Filter by lifetime" << '\n';
+//    std::cout << "2. Filter by nexthop address" << '\n';
+//    std::cout << "3. Filter by matching with prefix address" << '\n';
+//    std::cout << "4. Print filtered data structure" << '\n';
+//    std::cout << "5. Reset all filters" << '\n';
+//    std::cout << "6. Exit the program" << '\n';
+//    std::cout << "Enter your choice: ";
+//}
+//
+//void printFiltered(std::vector<RoutingRecord>& filteredVector) {
+//    for (RoutingRecord& record : filteredVector) {
+//        record.print();
+//    }
+//}
+//
+//int main() {
+//    RoutingTable rt("RT.csv");
+//    rt.printSize();
+//    std::vector<RoutingRecord> records = parseCSV("RT.csv");
+//	std::vector<RoutingRecord> workingSet = records;
+//    std::vector<RoutingRecord> filtered;
+//    int choice;
+//    std::string minimum, maximum, inputAdd, confirmation;
+//    int inputMask;
+//
+//    while (true) {
+//        showMenu();
+//        std::cin >> choice;
+//
+//        if (std::cin.fail()) {
+//            std::cin.clear(); 
+//            std::cin.ignore(10000, '\n'); 
+//            std::cout << "Invalid input. Please enter a number." << '\n';
+//            continue;
+//        }
+//
+//        switch (choice) {
+//        case 1:
+//            std::cout << "Enter minimum life time (example >> 1w4d3h or 6:25:20): ";
+//            std::cin >> minimum;
+//            std::cout << "Enter maximum life time (example >> 1w4d3h or 6:25:20): ";
+//            std::cin >> maximum;
+//            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
+//                [&](RoutingRecord& record) {
+//                    return isTimeInRange(record.getLifeTime(), minimum, maximum);
+//                });
+//            workingSet = filtered;
+//            printFiltered(workingSet);
+//            break;
+//
+//        case 2: 
+//            std::cout << "Enter a nexthop address (example >> 192.168.1.10): ";
+//            std::cin >> inputAdd;
+//            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
+//                [&](RoutingRecord& record) {
+//                    return record.getNextHopAdd() == inputAdd;
+//                });
+//            workingSet = filtered;
+//            printFiltered(workingSet);
+//            break;
+//
+//        case 3: 
+//            std::cout << "Enter address you want to match (example >> 192.168.1.10): ";
+//            std::cin >> inputAdd;
+//            std::cout << "Enter max prefix you want to match (1-31): ";
+//            std::cin >> inputMask;
+//            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
+//                [&](RoutingRecord& record) {
+//                    return doesIPMatch(record.getPrefixAddBits(), inputMask, ipToUint32(inputAdd));
+//                });
+//            workingSet = filtered;
+//            printFiltered(workingSet);
+//            break;
+//
+//        case 4: 
+//            std::cout << "Current filtered results (" << workingSet.size() << " records):" << '\n';
+//            printFiltered(workingSet);
+//            break;
+//
+//        case 5:
+//            workingSet = records;
+//            std::cout << "All filters reset. Working with full dataset (" << workingSet.size() << " records)." << '\n';
+//            break;
+//
+//        case 6: 
+//            std::cout << "Are you sure you want to exit? (y/n): ";
+//            std::cin >> confirmation;
+//            if (confirmation == "y" || confirmation == "Y") {
+//                std::cout << "Exiting...\n";
+//                return 0;
+//            }
+//            break;
+//
+//        default:
+//            std::cout << "Invalid choice. Try again.\n";
+//        }
+//
+//        if (choice >= 1 && choice <= 3) {
+//            std::cout << "Filter applied. Current results: " << workingSet.size() << " records." << '\n';
+//            std::cout << "Continue filtering? (y/n): ";
+//            std::cin >> confirmation;
+//            if (confirmation != "y" && confirmation != "Y") {
+//                workingSet = records;
+//            }
+//        }
+//    }
+//}
 
 int main() {
-    std::vector<RoutingRecord> records = parseCSV("RT.csv");
-	std::vector<RoutingRecord> workingSet = records;
-    std::vector<RoutingRecord> filtered;
-    int choice;
-    std::string minimum, maximum, inputAdd, confirmation;
-    int inputMask;
-
-    while (true) {
-        showMenu();
-        std::cin >> choice;
-
-        if (std::cin.fail()) {
-            std::cin.clear(); 
-            std::cin.ignore(10000, '\n'); 
-            std::cout << "Invalid input. Please enter a number." << '\n';
-            continue;
-        }
-
-        switch (choice) {
-        case 1:
-            std::cout << "Enter minimum life time (example >> 1w4d3h or 6:25:20): ";
-            std::cin >> minimum;
-            std::cout << "Enter maximum life time (example >> 1w4d3h or 6:25:20): ";
-            std::cin >> maximum;
-            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
-                [&](RoutingRecord& record) {
-                    return isTimeInRange(record.getLifeTime(), minimum, maximum);
-                });
-            workingSet = filtered;
-            printFiltered(workingSet);
-            break;
-
-        case 2: 
-            std::cout << "Enter a nexthop address (example >> 192.168.1.10): ";
-            std::cin >> inputAdd;
-            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
-                [&](RoutingRecord& record) {
-                    return record.getNextHopAdd() == inputAdd;
-                });
-            workingSet = filtered;
-            printFiltered(workingSet);
-            break;
-
-        case 3: 
-            std::cout << "Enter address you want to match (example >> 192.168.1.10): ";
-            std::cin >> inputAdd;
-            std::cout << "Enter max prefix you want to match (1-31): ";
-            std::cin >> inputMask;
-            filtered = Filter::filter(workingSet.begin(), workingSet.end(), 
-                [&](RoutingRecord& record) {
-                    return doesIPMatch(record.getPrefixAddBits(), inputMask, ipToUint32(inputAdd));
-                });
-            workingSet = filtered;
-            printFiltered(workingSet);
-            break;
-
-        case 4: 
-            std::cout << "Current filtered results (" << workingSet.size() << " records):" << '\n';
-            printFiltered(workingSet);
-            break;
-
-        case 5:
-            workingSet = records;
-            std::cout << "All filters reset. Working with full dataset (" << workingSet.size() << " records)." << '\n';
-            break;
-
-        case 6: 
-            std::cout << "Are you sure you want to exit? (y/n): ";
-            std::cin >> confirmation;
-            if (confirmation == "y" || confirmation == "Y") {
-                std::cout << "Exiting...\n";
-                return 0;
-            }
-            break;
-
-        default:
-            std::cout << "Invalid choice. Try again.\n";
-        }
-
-        if (choice >= 1 && choice <= 3) {
-            std::cout << "Filter applied. Current results: " << workingSet.size() << " records." << '\n';
-            std::cout << "Continue filtering? (y/n): ";
-            std::cin >> confirmation;
-            if (confirmation != "y" && confirmation != "Y") {
-                workingSet = records;
-            }
-        }
-    }
+	RoutingTable rt("RT.csv");
+	rt.printSize();
 }
